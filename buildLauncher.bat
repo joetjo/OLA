1aa5eff9e4515bb99916bfc23c289a4878203837
joetjo <joetjo@users.noreply.github.com>
2024-12-01 08:40:56 +0100

    V2024.12.1 updated

--- buildLauncher.bat
+++ buildLauncher.bat
@@ -1,5 +1,9 @@
+@ECHO OFF
 REM pip install pyinstaller
 pyinstaller --onefile --icon=joystick.ico ola-sbsgl-debug.py --add-data "./ola;./ola" --add-data "./base;./base"  --add-data "./resources;./resources" --add-data "./res;./res"  --add-data "./diskAnalyser;./diskAnalyser"  --add-data "./markdownHelper;./markdownHelper"  --add-data "./sbsgl;./sbsgl" --hidden-import=PySide6 --hidden-import=psutil  --hidden-import=json  --hidden-import=logging
 pyinstaller --onefile --noconsole --icon=joystick.ico ola-sbsgl.py --add-data "./ola;./ola" --add-data "./base;./base"  --add-data "./resources;./resources" --add-data "./res;./res"  --add-data "./diskAnalyser;./diskAnalyser"  --add-data "./markdownHelper;./markdownHelper"  --add-data "./sbsgl;./sbsgl" --hidden-import=PySide6 --hidden-import=psutil  --hidden-import=json  --hidden-import=logging
 REM
-echo "Don't forget to tag (git tag <tag> && git push --tags or select tag with PyCharm on push Window.)"
+echo ---
+echo Generation DONE
+echo ---
+echo "Dont forget to tag: git tag <tag> select tag with PyCharm on push Window."
